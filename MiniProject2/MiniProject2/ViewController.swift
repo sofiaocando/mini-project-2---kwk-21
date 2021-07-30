1d68e72ad80c21e82270cb63353e8653b0817baa
Sofia Ocando <88101688+sofiaocando@users.noreply.github.com>
2021-07-30 11:07:00 -0400

    Add files via upload
    mini project 2 - sofia ocando

---
+++ MiniProject2/MiniProject2/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var answerOutput1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func taylorSwiftButton(_ sender: Any) {
+        answerOutput1.text = "Great choice!Taylor is the music industry honestly"
+    }
+    
+    @IBAction func arianaGrandeButton(_ sender: Any) {
+        answerOutput1.text = "Mine too! Her music is amazing"
+    }
+    
+    @IBAction func oliviaRodrigoButton(_ sender: Any) {
+        answerOutput1.text = "Best new artist for sure!! You have so much taste"
+    }
+    
+    
+    
+}
+
